0327dcf373229a923d22548ba150bbf55311d1a0
hloc878@gmail.com <hloc878@gmail.com>
2022-08-10 10:52:31 +0700

    up

---
+++ js/main.js
@@ -0,0 +1,134 @@
+let dataGlasses = [
+  {
+    id: "G1",
+    src: "./img/g1.jpg",
+    virtualImg: "./img/v1.png",
+    brand: "Armani Exchange",
+    name: "Bamboo wood",
+    color: "Brown",
+    price: 150,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ea voluptates officiis? ",
+  },
+  {
+    id: "G2",
+    src: "./img/g2.jpg",
+    virtualImg: "./img/v2.png",
+    brand: "Arnette",
+    name: "American flag",
+    color: "American flag",
+    price: 150,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In assumenda earum eaque doloremque, tempore distinctio.",
+  },
+  {
+    id: "G3",
+    src: "./img/g3.jpg",
+    virtualImg: "./img/v3.png",
+    brand: "Burberry",
+    name: "Belt of Hippolyte",
+    color: "Blue",
+    price: 100,
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: "G4",
+    src: "./img/g4.jpg",
+    virtualImg: "./img/v4.png",
+    brand: "Coarch",
+    name: "Cretan Bull",
+    color: "Red",
+    price: 100,
+    description: "In assumenda earum eaque doloremque, tempore distinctio.",
+  },
+  {
+    id: "G5",
+    src: "./img/g5.jpg",
+    virtualImg: "./img/v5.png",
+    brand: "D&G",
+    name: "JOYRIDE",
+    color: "Gold",
+    price: 180,
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error odio minima sit labore optio officia?",
+  },
+  {
+    id: "G6",
+    src: "./img/g6.jpg",
+    virtualImg: "./img/v6.png",
+    brand: "Polo",
+    name: "NATTY ICE",
+    color: "Blue, White",
+    price: 120,
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: "G7",
+    src: "./img/g7.jpg",
+    virtualImg: "./img/v7.png",
+    brand: "Ralph",
+    name: "TORTOISE",
+    color: "Black, Yellow",
+    price: 120,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sint nobis incidunt non voluptate quibusdam.",
+  },
+  {
+    id: "G8",
+    src: "./img/g8.jpg",
+    virtualImg: "./img/v8.png",
+    brand: "Polo",
+    name: "NATTY ICE",
+    color: "Red, Black",
+    price: 120,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, unde enim.",
+  },
+  {
+    id: "G9",
+    src: "./img/g9.jpg",
+    virtualImg: "./img/v9.png",
+    brand: "Coarch",
+    name: "MIDNIGHT VIXEN REMIX",
+    color: "Blue, Black",
+    price: 120,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur soluta ad aut laborum amet.",
+  },
+];
+
+// getElement
+const getElement = (id) => {
+  return document.getElementById(id);
+};
+
+// list kính
+const listKinh = () => {
+  const kinh = dataGlasses.reduce((inital, value) => {
+    return (inital += `
+        <img class="col-4" id="${value.id}" src="${value.src}" alt="${value.name}" onclick="deoKinh('${value.id}')">
+    `);
+  }, ``);
+  getElement("vglassesList").innerHTML = kinh;
+};
+
+window.onload = () => {
+  listKinh();
+};
+
+window.deoKinh = (idClick) => {
+  dataGlasses.forEach((value) => {
+    if (value.id === idClick) {
+      getElement("glassesInfo").style.display = "block";
+      const img = `<img src="${value.virtualImg}" alt="${value.name}">`;
+      const htmlString = `
+        <h3>${value.name} - ${value.brand} (${value.color})</h3>
+        <span class="bg-danger text-light p-2 d-inline-block">$${value.price}</span>
+        <span class="text-success">Stocking</span>
+        <p class="text-light">${value.description}</p>
+        `;
+      getElement("avatar").innerHTML = img;
+      getElement("glassesInfo").innerHTML = htmlString;
+    }
+  });
+};
